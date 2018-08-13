7a41adbf89646803de8e6005a5139c180e1cb4c2
Alejandra Castillo <ahle.castillo@gmail.com>
2018-08-13 13:51:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  // your code here	  var index = 0;
+document.body.addEventListener("keydown", init)
+let index = 0
+function init(event) {
+  let key = event.which;
+  if (key === code[index]) {
+    index++
+    if (index === code.length) {
+      window.alert()
+    }
+  } else {
+    index = 0;
+  }
+ }
 }
+
